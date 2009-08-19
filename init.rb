deb5bc92aa1ffe6bb4889e39ab9693462a826389
Michael Dotterer <mdotterer@mdlogix.com>
2009-08-19 12:24:54 -0400

    Updating init.rb for name change

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
-require 'rvg_templates'
+require 'vector_view'
 
 Mime::Type.register "image/gif", :gif
 Mime::Type.register "image/png", :png
 Mime::Type.register "image/jpeg", :jpg
 
-ActionView::Template.register_template_handler 'rvg', RvgTemplates::TemplateHandler::Base
+ActionView::Template.register_template_handler 'rvg', VectorView::TemplateHandler::Base
